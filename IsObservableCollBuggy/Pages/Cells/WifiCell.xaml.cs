8756b88b0d1aaa29353c91e0cf63eaa9d3258cfd
L94\Artemio Gallardo <artemio.gallardo@xenex.com>
2021-05-28 16:31:51 -0500

    Added context action to the WifiCell

--- IsObservableCollBuggy/Pages/Cells/WifiCell.xaml.cs
+++ IsObservableCollBuggy/Pages/Cells/WifiCell.xaml.cs
@@ -1,9 +1,17 @@
-﻿using Xamarin.Forms;
+﻿using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace IsObservableCollBuggy.Pages.Cells
 {
     public partial class WifiCell : ViewCell
     {
+        public ICommand ForgetCommand
+        {
+            get => GetValue(ForgetCommandProperty) as ICommand;
+            set => SetValue(ForgetCommandProperty, value);
+        }
+        public static readonly BindableProperty ForgetCommandProperty = BindableProperty.Create(nameof(ForgetCommand), typeof(ICommand), typeof(WifiCell), null);
+
         public WifiCell()
         {
             InitializeComponent();
