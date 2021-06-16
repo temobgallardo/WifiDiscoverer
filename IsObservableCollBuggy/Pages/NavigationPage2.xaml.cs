492fb47b1914cb9593f8a300867ded5023159ea1
L94\Artemio Gallardo <artemio.gallardo@xenex.com>
2021-06-16 16:31:29 -0500

    Added two pages to test navigation in a IOT800 Arbor tablet

---
+++ IsObservableCollBuggy/Pages/NavigationPage2.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace IsObservableCollBuggy.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NavigationPage2 : ContentPage
+    {
+        public NavigationPage2()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new NavigationPage3());
+        }
+    }
+}
---
+++ IsObservableCollBuggy/Pages/NavigationPage3.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace IsObservableCollBuggy.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NavigationPage3 : ContentPage
+    {
+        public NavigationPage3()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+    }
+}
