d1d567d8d1347af8baa474fc84ebfaee80f8c3fa
L94\Artemio Gallardo <artemio.gallardo@xenex.com>
2021-07-13 17:37:40 -0500

    Renamed some pages and modified the navigation flow. Added BroadcastService to the Android.csproj

---
+++ IsObservableCollBuggy/Pages/Home.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace IsObservableCollBuggy.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Home : ContentPage
+    {
+        private WifiPageStructure _wifiPageStructure = new WifiPageStructure();
+        private Diagnostics _diagnostics = new Diagnostics();
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private async void ZappButton_Clicked(object s, EventArgs e)
+        {
+            //ZappSpinner.IsRunning = true;
+
+            //await Task.Delay(2000);
+
+            //ZappSpinner.IsRunning = false;
+
+            await Navigation.PushAsync(_wifiPageStructure);
+        }
+        private async void NavigateButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(_diagnostics);
+        }
+    }
+}
---
+++ IsObservableCollBuggy/Pages/WifiPAgeStructure.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace IsObservableCollBuggy.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WifiPageStructure : ContentPage
+    {
+        public WifiPageStructure()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+    }
+}
