ca617e523733b9120b9f51ccd9c066b6063c5082
Temo Gallardo <Artemio.Gallardo@xenex.com>
2021-07-19 00:45:56 -0500

    Added BeginAnimation trigger

---
+++ IsObservableCollBuggy/Triggers/BeginAnimation.cs
@@ -0,0 +1,17 @@
+﻿using IsObservableCollBuggy.Animations;
+using Xamarin.Forms;
+
+namespace IsObservableCollBuggy.Triggers
+{
+    [ContentProperty("Animation")]
+    public class BeginAnimation : TriggerAction<VisualElement>
+    {
+        public BaseAnimation Animation { get; set; }
+
+        protected override async void Invoke(VisualElement sender)
+        {
+            if (Animation != null)
+                await Animation.Begin();
+        }
+    }
+}
