d4fd468769ee5b1bb9b5decb6a1ac5fe328fad57
Temo Gallardo <Artemio.Gallardo@xenex.com>
2021-07-19 00:44:35 -0500

    Added Base and click Animation

---
+++ IsObservableCollBuggy/Animations/BaseAnimation.cs
@@ -0,0 +1,105 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace IsObservableCollBuggy.Animations
+{
+    public abstract class BaseAnimation : BindableObject
+    {
+        private CancellationTokenSource _animateTimerCancellationTokenSource;
+
+        public static readonly BindableProperty TargetProperty =
+            BindableProperty.Create(nameof(Target), typeof(VisualElement), typeof(BaseAnimation), null,
+                BindingMode.TwoWay, null);
+
+        public VisualElement Target
+        {
+            get { return (VisualElement)GetValue(TargetProperty); }
+            set { SetValue(TargetProperty, value); }
+        }
+
+        public static readonly BindableProperty DurationProperty =
+            BindableProperty.Create(nameof(Duration), typeof(string), typeof(BaseAnimation), "1000",
+                BindingMode.TwoWay, null);
+
+        public string Duration
+        {
+            get { return (string)GetValue(DurationProperty); }
+            set { SetValue(DurationProperty, value); }
+        }
+
+        public static readonly BindableProperty EasingProperty =
+            BindableProperty.Create(nameof(Easing), typeof(Easing), typeof(BaseAnimation), Easing.Linear,
+                BindingMode.TwoWay, null);
+
+        public Easing Easing
+        {
+            get { return (Easing)GetValue(EasingProperty); }
+            set { SetValue(EasingProperty, value); }
+        }
+
+        public static readonly BindableProperty DelayProperty =
+          BindableProperty.Create("Delay", typeof(int), typeof(BaseAnimation), 0, propertyChanged: (bindable, oldValue, newValue) =>
+              ((BaseAnimation)bindable).Delay = (int)newValue);
+
+        public int Delay
+        {
+            get { return (int)GetValue(DelayProperty); }
+            set { SetValue(DelayProperty, value); }
+        }
+
+        public static readonly BindableProperty RepeatForeverProperty =
+          BindableProperty.Create("RepeatForever", typeof(bool), typeof(BaseAnimation), false, propertyChanged: (bindable, oldValue, newValue) =>
+              ((BaseAnimation)bindable).RepeatForever = (bool)newValue);
+
+        public bool RepeatForever
+        {
+            get { return (bool)GetValue(RepeatForeverProperty); }
+            set { SetValue(RepeatForeverProperty, value); }
+        }
+
+        protected abstract Task BeginAnimation();
+
+        public async Task Begin()
+        {
+            if (Delay > 0)
+            {
+                await Task.Delay(Delay);
+            }
+
+            if (!RepeatForever)
+            {
+                await BeginAnimation();
+            }
+            else
+            {
+                RepeatAnimation(new CancellationTokenSource());
+            }
+        }
+
+        public void End()
+        {
+            ViewExtensions.CancelAnimations(Target);
+
+            if (_animateTimerCancellationTokenSource != null)
+            {
+                _animateTimerCancellationTokenSource.Cancel();
+            }
+        }
+
+        internal void RepeatAnimation(CancellationTokenSource tokenSource)
+        {
+            _animateTimerCancellationTokenSource = tokenSource;
+
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                if (!_animateTimerCancellationTokenSource.IsCancellationRequested)
+                {
+                    await BeginAnimation();
+
+                    RepeatAnimation(_animateTimerCancellationTokenSource);
+                }
+            });
+        }
+    }
+}
---
+++ IsObservableCollBuggy/Animations/ClickAnimation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace IsObservableCollBuggy.Animations
+{
+    public class ClickAnimation : BaseAnimation
+    {
+        public float Scale
+        {
+            get => (float)GetValue(ScaleProperty);
+            set => SetValue(ScaleProperty, value);
+        }
+        public static BindableProperty ScaleProperty = BindableProperty.Create(
+            propertyName: "ScaleProperty",
+            returnType: typeof(float),
+            declaringType: typeof(ClickAnimation),
+            defaultValue: 1.0f);
+
+        protected override Task BeginAnimation()
+        {
+            if (Target == null)
+            {
+                throw new NullReferenceException("Null Target property.");
+            }
+
+            return Task.Run(() =>
+            {
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    Target.Animate("BounceIn", ScaleUpAndDown(), 16, Convert.ToUInt32(Duration));
+                });
+            });
+        }
+
+        internal Animation ScaleUpAndDown()
+        {
+            var animate = new Animation();
+            var scaleUp = new Animation(v => Target.Scale = v, 1, Scale);
+            var scaleDown = new Animation(v => Target.Scale = v, Scale, 1);
+
+            animate.Add(0, 0.5, scaleUp);
+            animate.Add(0.5, 1, scaleDown);
+
+            return animate;
+        }
+    }
+}
