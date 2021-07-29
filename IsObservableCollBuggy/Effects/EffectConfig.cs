f4456f2e17c8122e93a9cb7e81d29a6106438085
L94\Artemio Gallardo <artemio.gallardo@xenex.com>
2021-07-29 16:41:37 -0500

    Added touch effect

--- IsObservableCollBuggy/Effects/EffectConfig.cs
+++ IsObservableCollBuggy/Effects/EffectConfig.cs
@@ -1,7 +1,63 @@
-﻿namespace IsObservableCollBuggy.Effects
+﻿using Xamarin.Forms;
+
+namespace IsObservableCollBuggy.Effects
 {
     public static class EffectConfig
     {
-        public static bool EnableTriggerProperty { get; set; } = true;
+        public static class EffectsConfig
+        {
+            public static bool AutoChildrenInputTransparent { get; set; } = true;
+
+            public static readonly BindableProperty ChildrenInputTransparentProperty =
+                BindableProperty.CreateAttached(
+                    propertyName: "ChildrenInputTransparent",
+                    returnType: typeof(bool),
+                    declaringType: typeof(EffectsConfig),
+                    defaultValue: false,
+                    propertyChanged: (bindable, oldValue, newValue) => {
+                        ConfigureChildrenInputTransparent(bindable);
+                    }
+                );
+
+            public static void SetChildrenInputTransparent(BindableObject view, bool value)
+            {
+                view.SetValue(ChildrenInputTransparentProperty, value);
+            }
+
+            public static bool GetChildrenInputTransparent(BindableObject view)
+            {
+                return (bool)view.GetValue(ChildrenInputTransparentProperty);
+            }
+
+            static void ConfigureChildrenInputTransparent(BindableObject bindable)
+            {
+                if (!(bindable is Layout layout))
+                    return;
+
+                if (GetChildrenInputTransparent(bindable))
+                {
+                    foreach (var layoutChild in layout.Children)
+                        AddInputTransparentToElement(layoutChild);
+                    layout.ChildAdded += Layout_ChildAdded;
+                }
+                else
+                {
+                    layout.ChildAdded -= Layout_ChildAdded;
+                }
+            }
+
+            static void Layout_ChildAdded(object sender, ElementEventArgs e)
+            {
+                AddInputTransparentToElement(e.Element);
+            }
+
+            static void AddInputTransparentToElement(BindableObject obj)
+            {
+                if (obj is VisualElement view && Touch.GetColor(view) == Color.Default)
+                {
+                    view.InputTransparent = true;
+                }
+            }
+        }
     }
 }
--- IsObservableCollBuggy/Effects/Touch.cs
+++ IsObservableCollBuggy/Effects/Touch.cs
@@ -1,6 +1,68 @@
-﻿namespace IsObservableCollBuggy.Effects
+﻿using System.Linq;
+using Xamarin.Forms;
+using static IsObservableCollBuggy.Effects.EffectConfig;
+
+namespace IsObservableCollBuggy.Effects
 {
-    class Touch
+    public class TouchRoutingEffect : RoutingEffect
     {
+        public TouchRoutingEffect() : base($"WifiPage.Effects.{nameof(TouchRoutingEffect)}")
+        {
+        }
+    }
+
+    public static class Touch
+    {
+        public static readonly BindableProperty ColorProperty = BindableProperty.CreateAttached(
+               propertyName: "Color",
+               returnType: typeof(Color),
+               declaringType: typeof(TouchRoutingEffect),
+               defaultValue: Color.Default,
+               propertyChanged: TryGenerateEffect
+           );
+
+        public static void SetColor(BindableObject view, Color value)
+        {
+            view.SetValue(ColorProperty, value);
+        }
+
+        public static Color GetColor(BindableObject view)
+        {
+            return (Color)view.GetValue(ColorProperty);
+        }
+
+        static void TryGenerateEffect(BindableObject bindable, object oldValue, object newValue)
+        {
+            if (!(bindable is VisualElement element))
+                return;
+
+            var eff = element.Effects.FirstOrDefault(e => e is TouchRoutingEffect);
+            if (GetColor(bindable) != Color.Default || GetColor(bindable) != null)
+            {
+                element.InputTransparent = false;
+
+                if (eff != null) return;
+
+                element.Effects.Add(new TouchRoutingEffect());
+
+                if (EffectsConfig.AutoChildrenInputTransparent && bindable is Layout &&
+                    !EffectsConfig.GetChildrenInputTransparent(element))
+                {
+                    EffectsConfig.SetChildrenInputTransparent(element, true);
+                }
+            }
+            else
+            {
+                if (eff == null || element.BindingContext == null) return;
+
+                element.Effects.Remove(eff);
+
+                if (EffectsConfig.AutoChildrenInputTransparent && bindable is Layout &&
+                    EffectsConfig.GetChildrenInputTransparent(element))
+                {
+                    EffectsConfig.SetChildrenInputTransparent(element, false);
+                }
+            }
+        }
     }
 }
