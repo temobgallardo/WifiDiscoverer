716ddb49036b5337ef1cdbed2bf2f2b598199cb5
L94\Artemio Gallardo <artemio.gallardo@xenex.com>
2021-08-03 16:37:32 -0500

    Added elements for testing the gradient in labels.

--- IsObservableCollBuggy/Pages/Home.xaml.cs
+++ IsObservableCollBuggy/Pages/Home.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IsObservableCollBuggy.Effects;
+using System;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -10,6 +11,7 @@ namespace IsObservableCollBuggy.Pages
     {
         private readonly WifiPageStructure _wifiPageStructure = new WifiPageStructure();
         private readonly Diagnostics _diagnostics = new Diagnostics();
+        private readonly Random _rand = new Random();
         public Home()
         {
             InitializeComponent();
@@ -36,6 +38,23 @@ namespace IsObservableCollBuggy.Pages
         protected override void OnDisappearing()
         {
             base.OnDisappearing();
+        }
+
+        private void ChangeLabelGradientColor_Clicked(object sender, EventArgs e)
+        {
+            var colors = new GradientColors(new Color[] { GetRandomColor(), GetRandomColor() });
+            //Gradient.SetColors(GradientLabel, colors);
+            Gradient.SetColors(GradientAndTouchLabel, colors);
+            //Gradient.SetColors(GradientFrame, colors);
+        }
+
+        private Color GetRandomColor() => Color.FromRgb(_rand.Next(0, 255), _rand.Next(0, 255), _rand.Next(0, 255));
+
+        private void ChangeTouchColor_Clicked(object sender, EventArgs e)
+        {
+            var colors = new GradientColors(new Color[] { GetRandomColor(), GetRandomColor() });
+            Gradient.SetColors(GradientAndTouchLabel, colors);
+            Gradient.SetTouchColor(GradientAndTouchLabel, GetRandomColor());
         }
     }
 }
